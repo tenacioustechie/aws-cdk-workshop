0d50bbd651c5ab14d833ea851a03f59e11baa019
Brian Mills <brian@millsit.com>
2019-10-20 06:58:52 +1100

    initial stack

--- bin/aws-cdk-workshop.ts
+++ bin/aws-cdk-workshop.ts
@@ -2,5 +2,6 @@
 import cdk = require('@aws-cdk/core');
 import { AwsCdkWorkshopStack } from '../lib/aws-cdk-workshop-stack';
 
+// App Entry Point
 const app = new cdk.App();
 new AwsCdkWorkshopStack(app, 'AwsCdkWorkshopStack');
--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -3,6 +3,7 @@ import subs = require('@aws-cdk/aws-sns-subscriptions');
 import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
 
+// This is the App Definition class created by the entry point in bin folder
 export class AwsCdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
