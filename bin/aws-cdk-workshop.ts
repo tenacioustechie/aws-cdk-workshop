49c6e873a41d18e2af5bbcb263f4ca47392503ea
Brian Mills <brian@millsit.com>
2023-09-14 17:03:59 +1000

    Added code pipeline code

--- bin/aws-cdk-workshop.ts
+++ bin/aws-cdk-workshop.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { AwsCdkWorkshopStack } from '../lib/aws-cdk-workshop-stack';
+import * as cdk from "aws-cdk-lib";
+import { AwsCdkWorkshopStack } from "../lib/aws-cdk-workshop-stack";
+import { WorkshopPipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
-new AwsCdkWorkshopStack(app, 'AwsCdkWorkshopStack');
+new WorkshopPipelineStack(app, "AwsCdkWorkshopStack");
