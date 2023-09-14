4962287f78c903e0603deb4ff53078862d00c487
Brian Mills <brian@millsit.com>
2023-09-14 18:59:37 +1000

    Fixed pipeline stack file

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,4 +1,4 @@
-import { CdkWorkshopStack } from "./cdk-workshop-stack";
+import { AwsCdkWorkshopStack } from "./aws-cdk-workshop-stack";
 import { Stage, StageProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
@@ -6,6 +6,6 @@ export class WorkshopPipelineStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
-    new CdkWorkshopStack(this, "WebService");
+    new AwsCdkWorkshopStack(this, "WebService");
   }
 }
